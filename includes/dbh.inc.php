cc55997747fcd9caf3745e9c2d37974ebdfa482d
Darrell Grant <darrellgrantinfo@gmail.com>
2021-05-15 20:56:20 -0400

    Delete dbh.inc.php

--- includes/dbh.inc.php
+++
@@ -1,7 +0,0 @@
-<?php
-$dbServername ="localhost";
-$dbUsername ="root";
-$dbPassword ="Brainiac5";
-$dbName ="loginsystem";
-
-$conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName );
